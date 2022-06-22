d9a367c5c531ca8832e05d65f0d7adb3ed5754dd
NovusAlvarez <105301865+NovusAlvarez@users.noreply.github.com>
2022-06-22 11:58:35 +0300

    Update Judy_Bot.py

--- Judy_Bot.py
+++ Judy_Bot.py
@@ -1,10 +1,12 @@
 import telebot;
 from telebot import types
-bot = telebot.TeleBot('WEE-WEE');
+
+bot = telebot.TeleBot('no ;)')
 
 name = '';
 surname = '';
 age = 0;
+
 
 @bot.message_handler(content_types=['text'])
 def start(message):
@@ -14,11 +16,13 @@ def start(message):
     else:
         bot.send_message(message.from_user.id, 'Блин, Чумба! Я же регаю тебя, давай по делу. Напиши /reg');
 
+
 def get_name(message):
     global name;
     name = message.text;
     bot.send_message(message.from_user.id, 'Записала. Какая у тебя фамилия?');
     bot.register_next_step_handler(message, get_surname);
+
 
 def get_surname(message):
     global surname;
@@ -26,29 +30,32 @@ def get_surname(message):
     bot.send_message(message.from_user.id, 'Ага. Сколько тебе лет?');
     bot.register_next_step_handler(message, get_age);
 
+
 def get_age(message):
     global age;
     while age == 0:
         try:
             age = int(message.text)
         except Exception:
-            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
-            keyboard = types.InlineKeyboardMarkup();
-            key_yes = types.InlineKeyboardButton(text='Да', callback_data='no');
-            keyboard.add(key_yes);
-            key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
-            keyboard.add(key_no);
-            question = 'Так, вроде это всё, давай проверим: Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
-            bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+    keyboard = types.InlineKeyboardMarkup();
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='no');
+    keyboard.add(key_yes);
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no');
+    keyboard.add(key_no);
+    question = 'Так, вроде это всё, давай проверим: Тебе ' + str(
+        age) + ' лет, тебя зовут ' + name + ' ' + surname + '?'
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "yes":
-        bot.send_message(call.message.chat.id, 'Супер! Чуть позже отправлю твои данные в систему и будешь ходить там как свой ;)')
+        bot.send_message(call.message.chat.id, 'Супер! Чуть позже отправлю твои данные в систему и будешь ходить там как свой ;)');
     elif call.data == "no":
         bot.polling(none_stop=True, interval=0)
-def main():
-    Bot.pooling()
 
 
+if __name__ == '__main__':
+    bot.polling()
 
